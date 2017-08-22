221a41daa21a8ca261c39eb3e55b44b97b2ca4dc
MrRudenko <rudymon96@gmail.com>
2017-08-22 18:52:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function passs(){
+	if(document.rd.i2.value != 'qwerty123'){document.getElementById('p2').innerHTML = 'Ok! New pass: ' + document.rd.i2.value;}
+	else{document.getElementById('p2').innerHTML = 'Yeap!';}
+}
+
+
+
